0fffde4b94ee556ee7a0ae56d5d22a13d84e2478
Richard Searle <harold.richard.searle@gmail.com>
2015-07-16 17:21:49 -0500

    fix compile errors

--- src/test/scala/pcap/streams/Streamer.scala
+++ src/test/scala/pcap/streams/Streamer.scala
@@ -4,9 +4,10 @@ import java.io.File
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.io.SynchronousFileSource
-import pcap.codec.Codecs.Wrapper
+import pcap.codec.Codecs.WithHeaderDecoder
 import pcap.data.Data
-
+import scodec.bits.ByteVector
+import akka.stream.scaladsl.Sink
 
 /**
  * @author rsearle
@@ -16,13 +17,25 @@ object Streamer extends App {
   import system.dispatcher
   implicit val materializer = ActorMaterializer()
 
-  val f = SynchronousFileSource(new File("/tmp/dump.pcap"))
-    .transform(() => DecoderStage(new Wrapper))
+  val s =SynchronousFileSource(new File("/tmp/simple.pcap"))
+  val f = s
+  .map(x => {println(x);x})
+    .transform(() => ByteStringDecoderStage(new WithHeaderDecoder))
     .filter(_ match {
-      case data: Data if !data.bytes.isEmpty => true
-      case _                                 => false
-    })
-    .runForeach { println }
+      case data: pcap.data.v6.TCP /*if !data.bytes.isEmpty*/ => true
+      case _ => false
+   })
+    .map { _.asInstanceOf[pcap.data.v6.TCP] }
+    .groupBy(_.stream)
+    .map {
+      case (key, s) => (key, s.runFold(ByteVector.empty) { (bv, t) => bv ++ t.bytes })
+    }
+   
+  //  .to(Sink.ignore).run()
+   // .runForeach { r =>  r._2.map { bv => println(r._1, bv.decodeUtf8.fold(_.toString,_.toString)) } }
+ .runForeach {println}
     .onComplete { case _ => system.shutdown }
+    
+   
 
 }
